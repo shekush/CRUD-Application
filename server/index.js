0c0e813af180826c931eb25c5f55b615f6372461
Shejal <shejal270999@gmail.com>
2022-08-22 23:33:53 +0530

    connected the app

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const mongoose = require(mongoose);
 const app = express();
 
 
+app.use(express.json);
 
+mongoose.connect("mongodb+srv://shejal:shejal12345@crud.legj357.mongodb.net/?retryWrites=true&w=majority", 
+{
+    useNewUrlParser: true,
+});
 
 
 
